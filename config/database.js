4ae6f79f8ea75bae816e8cface8b4be76974982b
vinnykaka7 <ngatiavincent6@gmail.com>
2025-11-22 10:16:04 +0300

    Change MongoDB connection URI in database.js
    Updated MongoDB connection URI with credentials.

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI="mongodb+srv://:ngatiavincent6_db_user:kaka456@cluster0.abcde.mongodb.net/OrganDonationDB?retryWrites=true&w=majority");
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error('❌ MongoDB connection error:', error.message);
